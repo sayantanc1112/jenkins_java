7ab89d5ffc87fc3b1d17130351a3db0d2ae994b4
sayantanc1112 <schowdhury1112@gmail.com>
2023-07-10 20:31:29 +0530

    Second Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        System.out.println("This java file is automated to run via Jenkins from github directly!"); 
     }
 }
